9c3b8d89630e15c0b5295fd953c588a89e868c21
Rafael Leira Osuna <rafael.leira@estudiante.uam.es>
2015-06-08 17:42:50 +0200

    El script de autocompilación comprueba que estén instalados los programas necesarios.

--- autocompilar.sh
+++ autocompilar.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+command -v dnotify >/dev/null 2>&1 || { echo >&2 "Para utilizar este Script debe instalar el programa 'dnotify'. Pruebe con: 'sudo apt-get install dnotify'."; exit 1; }
+
 while [ 1 ]
 do
+	echo "Esperando modificaciones en su .tex ... [Presione Ctrl+C para parar la ejecucion]"
+
 	dnotify -Mso src .
 
-	echo "recompiling... [Presione Cntrl+C para parar la ejecucion]"
+	echo "Recompilando... [Presione Cntrl+C para parar la ejecucion]"
 
 	./compilar.sh
 
