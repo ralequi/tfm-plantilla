92237e953035ad0d456f48bdd11b9ffcd5dc887a
Rafael Leira Osuna <rafael.leira@estudiante.uam.es>
2015-06-08 13:13:38 +0200

    Actualizados los scripts de compilación. Modificados los .tex de forma que se cambió grado por master

--- autocompilar.bat
+++ autocompilar.bat
@@ -2,11 +2,13 @@
 :init
 cls
 del *alg *acr *acn *lof *log *lot *out *bak *toc *xdy *ist *gls *glo *blg *aux *bbl
-pdflatex -interaction=nonstopmode tfg.tex > nul
-bibtex tfg > nul
-makeglossaries tfg > nul
-pdflatex -interaction=nonstopmode tfg.tex > nul 
-pdflatex -interaction=nonstopmode tfg.tex > nul
+pdflatex -interaction=nonstopmode main.tex > nul
+bibtex main > nul
+makeglossaries main > nul
+pdflatex -interaction=nonstopmode main.tex > nul 
+pdflatex -interaction=nonstopmode main.tex > nul
+
+move main.pdf "Trabajo de Fin de Master.pdf"
 
 pause
 goto init
--- compilar.sh
+++ compilar.sh
@@ -9,7 +9,7 @@ bibtex main
 makeglossaries main
 pdflatex main.tex
 pdflatex main.tex
-mv main.pdf "Trabajo de Fin de Grado.pdf"
+mv main.pdf "Trabajo de Fin de Master.pdf"
 
 # Limpiar archivos auxiliares
 ./limpiar.sh
