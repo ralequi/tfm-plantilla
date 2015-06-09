a020a6c326f590393fca4a65a8a9ca67bed316ee
Rafael Leira Osuna <rafael.leira@estudiante.uam.es>
2015-06-09 14:57:33 +0200

    Los scripts de compilación, son un poco más inteligentes y muestran solo la información necesaria.

--- compilar.sh
+++ compilar.sh
@@ -1,12 +1,46 @@
 #!/bin/bash
 
 # Compilar
-pdflatex -interaction=nonstopmode main.tex
-bibtex main
-makeglossaries main
-pdflatex -interaction=nonstopmode main.tex
-pdflatex -interaction=nonstopmode main.tex
+echo -n $'Compilando... pdflatex (1/3)\r'
+pdflatex -interaction=nonstopmode main.tex > /dev/null
+
+echo -n $'Compilando... bibtex\r'
+bibtex main > /tmp/bibtex.log
+
+echo -n $'Compilando... makeglossaries\r'
+makeglossaries main > /tmp/makeglossaries.log
+
+echo -n $'Compilando... pdflatex (2/3)\r'
+pdflatex -interaction=nonstopmode main.tex > /dev/null
+
+echo -n $'Compilando... pdflatex (3/3)\r'
+pdflatex -interaction=nonstopmode main.tex > /tmp/pdflatex.log
 mv main.pdf "Trabajo de Fin de Master.pdf"
 
 # Limpiar archivos auxiliares
+echo -n $'Limpiando...\r'
 ./limpiar.sh
+
+echo -n $'Compilacion terminada. '; date
+
+#Latex Errors
+if grep -q '!' /tmp/pdflatex.log
+then
+	echo ""
+	echo -e "Latex Errors: \e[31m\e[1m"
+	grep '!' -n -A1 /tmp/pdflatex.log
+
+	#Return to normal color
+	echo -e "\e[39m\e[0m"
+fi
+
+#BibTex Errors
+if grep -q 'There was' /tmp/bibtex.log
+then
+	echo ""
+	echo -e "Bibtex Errors! Check log: \e[95m\e[1m"
+	cat /tmp/bibtex.log
+
+	#Return to normal color
+	echo -e "\e[39m\e[0m"
+fi
