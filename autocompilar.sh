fc60d2ba25283db19fe1fb26d93c13b137d04d5f
Rafael Leira Osuna <rafael.leira@estudiante.uam.es>
2015-06-15 11:26:25 +0200

    Ahora autocompilar, compila antes y luego espera

--- autocompilar.sh
+++ autocompilar.sh
@@ -5,13 +5,10 @@ command -v dnotify >/dev/null 2>&1 || { echo >&2 "Para utilizar este Script debe
 clear
 while [ 1 ]
 do
-	echo "Esperando modificaciones en su .tex ... [Presione Ctrl+C para parar la ejecucion]"
-
-	dnotify -Mso src .
-
 	echo "Recompilando... [Presione Cntrl+C para parar la ejecucion]"
-
 	./compilar.sh
-
 	echo
+
+	echo "Esperando modificaciones en su .tex ... [Presione Ctrl+C para parar la ejecucion]"
+	dnotify -Mso src .
 done
